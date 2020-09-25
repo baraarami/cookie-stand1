eb948f157cc9bfb306d6048b935c0dc1f99472a8
AyaAbuRashed <ayanaser402@gmail.com>
2020-09-26 02:49:17 +0300

    lab9 DONE

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var tableEl = document.getElementById("table1");
+console.log(tableEl);
 workHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 function Location(lname,minCustomerHour, maxCustomerHour, average) {  this.lname=lname;
     this.lname=lname;
@@ -22,6 +23,8 @@ function Location(lname,minCustomerHour, maxCustomerHour, average) {  this.lname
     }
 };
   
+
+
   }
   function getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is exclusive and the minimum is inclusive
@@ -44,15 +47,7 @@ d.getcookiesPerHour();
 e.getNum();
 e.getcookiesPerHour();
   
-  /*document.writeln("Seattle",a.hours);
-  document.writeln("br");
-  document.writeln("Tokyo",a.hours);
-  document.writeln("br");
-  document.writeln("Dubai",a.hours);
-  document.writeln("br");
-  document.writeln("Paris",a.hours);
-  document.writeln("br");
-  document.writeln("Lima",a.hours);*/
+  
 
 function createHeader()
 {    ///create the header of the table
@@ -95,9 +90,6 @@ var rowData2 =document.createElement('td');
     row.appendChild(rowData2);
     rowData2.textContent=this.total;
 
-
- 
-    
 }
 function creatFooter()
 {var totalofTotal2=0;
@@ -128,17 +120,77 @@ d.render();
 e.render();
 creatFooter(a,b,c,d,e);
 
-function myfunction()
-{  a=document.getElementById("lname");
-   b=document.getElementById("min");
-   b=parseInt(b);
-   c=document.getElementById("max");
-   c=parseInt(c);
-   d=document.getElementById("av");
-   d=parseInt(d);
-   var add = new Location(a,b,c,d);
+form1.addEventListener('submit' , function (event){
+   event.preventDefault();
+  
+    var loc = event.target.lname.value;
+    console.log(loc);
+    var minN = event.target.min.value;
+    var maxN = event.target.max.value;
+    var aveN = event.target.ave.value;
+  var add = new Location (loc,minN,maxN,aveN);
+  var rowNum =tableEl.rows.length;
+  tableEl.deleteRow(rowNum-1);
+  
   add.getNum();
   add.getcookiesPerHour();
   add.render();
+  creatFooter(a,b,c,d,e,add);
+  form1.reset();
+  });
+    
 
-}
+
+// form.addEventListener('submit' , function (event){
+//   console.log(event.target);
+//   var loc =event.target.lname.value;
+//   console.log(loc);
+    // var minN =document.getElementById("min");
+    // minN=parseInt(minN);
+    // console.log("aya",minN);
+    // var maxN=document.getElementById("max");
+    // maxN=parseInt(maxN);
+    // var aveN=document.getElementById("av");
+    // aveN=parseInt(aveN);
+    // var add = new Location(loc,minN,maxN,aveN);
+    // add.getNum();
+    // add.getcookiesPerHour();
+    // add.render();
+    // creatFooter(a,b,c,d,e,add);
+
+
+
+
+
+// function AddRow (event){
+
+//   var loc =document.getElementById("lname");
+//   var minN =document.getElementById("min");
+//   minN=parseInt(minN);
+//   console.log("aya",minN);
+//   var maxN=document.getElementById("max");
+//   maxN=parseInt(maxN);
+//   var aveN=document.getElementById("av");
+//   aveN=parseInt(aveN);
+//   var add = new Location(loc,minN,maxN,aveN);
+//   add.getNum();
+//   add.getcookiesPerHour();
+//   add.render();
+//   creatFooter(a,b,c,d,e,add);
+// }
+
+
+// function myfunction()
+// {  a=document.getElementById("lname");
+//    b=document.getElementById("min");
+//    b=parseInt(b);
+//    c=document.getElementById("max");
+//    c=parseInt(c);
+//    d=document.getElementById("av");
+//    d=parseInt(d);
+//    var add = new Location(a,b,c,d);
+//   add.getNum();
+//   add.getcookiesPerHour();
+//   add.render();
+
+// }
